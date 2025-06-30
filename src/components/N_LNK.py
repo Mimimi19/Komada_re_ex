d66e0a6e849636f3b4362fcac929921e7abce6f1
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2025-06-30 12:01:28 +0900

    非線形関数の定義

---
+++ src/components/N_LNK.py
@@ -0,0 +1,17 @@
+
+from math import erf
+
+
+def main(x, a, b1, b2, kappa, ka):
+    """
+    非線形性 NLNK(x) を定義します（式14）。
+    x: フィルターされた刺激 g(t) の値
+    a, b1, b2: 非線形性のパラメータ
+    kappa: フィルターされた刺激 g(t) の分散 [7]
+    ka: 活性化率定数（動力学ブロックのパラメータ）
+    """
+    return a ** (erf(kappa * x + b1) + 1 ) * ka ** (- 1) + b2
+
+if __name__ == "__main__":
+    # テスト用のパラメータ
+    a = 1.0
